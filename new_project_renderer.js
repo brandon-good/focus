7ab680ac3d5abb60e83aff381c43405c6b465e73
brandon-good <brandongood@email.arizona.edu>
2024-02-21 22:44:11 -0700

    new folder option in dialog from select_src

--- new_project_renderer.js
+++ new_project_renderer.js
@@ -1,11 +1,7 @@
-const dialogConfig = {
-    title: 'Choose Directory',
-    buttonLabel: 'Choose Directory',
-    properties: ['openDirectory']
-};
-const src_btn  = document.getElementById("#src_dir_btn")
+
+const src_btn = document.getElementById("#src_dir_btn")
 const dest_btn = document.getElementById("#dest_dir_btn")
-const src_display  = document.getElementById("#src_dir_display");
+const src_display = document.getElementById("#src_dir_display");
 const dest_display = document.getElementById("#dest_dir_display");
 const name_field = document.getElementById("#project_name");
 const create_btn = document.getElementById("#new_project_create_btn")
@@ -14,8 +10,13 @@ const cancel_btn = document.getElementById("#new_project_cancel_btn")
 let src_dir;
 let dest_dir;
 
+const dialogConfig = {
+	title: 'Choose Directory',
+	buttonLabel: 'Choose Directory',
+	properties: ['openDirectory']
+};
 function select_src(event) {
-	electron.dialog('showOpenDialog', dialogConfig).then(import_dir => {
+	electron.dialog('showOpenDialog', { title: 'Choose Source Directory', buttonLabel: "Choose Directory", properties: ['openDirectory', 'createDirectory'] }).then(import_dir => {
 		if (!import_dir.canceled) {
 			src_dir = import_dir.filePaths[0];
 			src_display.innerText = src_dir;
@@ -33,8 +34,8 @@ function select_dest(event) {
 }
 
 function create_project(event) {
-	let name = name_field.value;	
-	ipcRenderer.send('create_new_project', {name, src_dir, dest_dir, })
+	let name = name_field.value;
+	ipcRenderer.send('create_new_project', { name, src_dir, dest_dir, })
 }
 
 function cancel_project(event) {
