a40c06d2bb8cbd6f1c46f65df5b5020a5756097d
Sean Eddy <tikkisean@gmail.com>
2024-04-22 15:57:02 -0700

    Fixed status bug

--- src/Status.js
+++ src/Status.js
@@ -7,18 +7,20 @@ export default function Status({ projects }) {
 				id="status-icon"
 				style={{
 					background: projects.some(
-						(project) => project.loading || project.exporting
+						(project) => project.open && (project.loading || project.exporting)
 					)
 						? "red"
 						: "green",
 				}}
 			></div>
 			<span>
-				{projects.some((project) => project.loading && project.archived)
+				{projects.some(
+					(project) => project.open && project.loading && project.archived
+				)
 					? "Unarchiving project..."
-					: projects.some((project) => project.loading)
+					: projects.some((project) => project.open && project.loading)
 					? "Creating project..."
-					: projects.some((project) => project.exporting)
+					: projects.some((project) => project.open && project.exporting)
 					? "Exporting project..."
 					: "Ready"}
 			</span>
